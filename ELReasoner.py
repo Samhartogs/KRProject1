5797d8a58014a947a3b2dec3b43309d855c28f50
Sam <hartogssam@gmail.com>
2023-11-19 16:10:19 +0100

    .

--- ELReasoner.py
+++ ELReasoner.py
@@ -36,7 +36,6 @@ gateway.convertToBinaryConjunctions(ontology)
 # get the TBox axioms
 tbox = ontology.tbox()
 axioms = tbox.getAxioms()
-changed = False
 
 
 print("These are the axioms in the TBox:")
@@ -52,41 +51,90 @@ print("These are the concept names: ")
 print([formatter.format(x) for x in conceptNames])
 test = elFactory.getConceptName(classname)
 
+class reasoner:
+    def __init__(self):
+        self.changed = True
+        self.elements = []
+        self.counter = 0
 
-def conjunction1(concept):
-    conceptType = concept.getClass().getSimpleName()
-    if conceptType == "ConceptConjunction":
-        for conjunct in concept.getConjuncts():
-            concept.append(conjunct)
-    changed = True
-    return concept
+    def conjunction1(self, concept, e):
+        conceptType = concept.getClass().getSimpleName()
+        if conceptType == "ConceptConjunction":
+            for conjunct in concept.getConjuncts():
+                e.append(conjunct)
+            self.changed = True
+        return e
 
-def rules(concept):
-    #Trule(concept)
-    conjunction1(concept)
-    #concept.append(conjunction2(concept))
-    #existential1(concept)
-    #existential2(concept)
-    #subsume(concept)
+    def subsume(self, concept, e):
+        for axiom in axioms:
+            axiomType = axiom.getClass().getSimpleName()
+            if axiomType == "GeneralConceptInclusion" and axiom.lhs() == concept:
+                if axiom.rhs() not in e:
+                    e.append(axiom.rhs())
+                    self.changed = True
+        return e
 
-def ELreasoner(classname):
-    subsumer = []
-    for concept in conceptNames:
-        d = [classname]
-        elements = [d]
-        changed = True
-        while changed == True:
-            changed = False
-            for e in elements:
-                for _ in e:
-                    rules(_)
-        if concept in d:
-            subsumer.append(concept)
-    return subsumer
+    def conjunction2(self, concept, e):
+        for axiom in axioms:
+            axiomType = axiom.getClass().getSimpleName()
+            if axiomType == "GeneralConceptInclusion":
+                conceptType = axiom.rhs().getClass().getSimpleName()
+                if conceptType == "ConceptConjunction":
+                    conjunct = axiom.rhs().getConjuncts()
+                    print(conjunct)
+                    if all(element in conjunct for element in e):
+                        e.append(elFactory.getConjunction(conjuct[0], conjunct[1]))
+                        self.changed = True
+        return e
 
-subsumers = ELreasoner(test)
+    def existential1(self, concept, elements):
+        conceptType = concept.getClass().getSimpleName()
+        if conceptType == "ExistentialRoleRestriction":
+            for element in elements:
+                if concept.filler() in element[0]:
+                    return elements
+            d = []
+            d.append(concept.filler())
+            elements.append(d)
+        return elements
+
+    def existential2(self, elements):
+        conceptType = concept.getClass().getSimpleName()
+        if conceptType == "ExistentialRoleRestriction":
+
+
+
+
+
+
+
+    def rules(self, concept, e, elements):
+        #Trule(concept)
+        #self.conjunction1(concept, e)
+        #self.conjunction2(concept, e)
+        #self.existential1(concept, elements)
+        self.existential2(concept, e)
+        #self.subsume(concept, e)
+
+    def ELreasoner(self, classname):
+        subsumer = []
+        for concept in conceptNames:
+            d = [classname]
+            self.elements = [d]
+            self.changed = True
+            while self.changed == True:
+                self.changed = False
+                for e in self.elements:
+                    for _ in e:
+                        self.rules(_, e, self.elements)
+            if concept in d:
+                subsumer.append(concept)
+        return subsumer
+
+subsumers = reasoner()
+result = subsumers.ELreasoner(test)
 print(f"According to our reasoner, {test} has the following subsumers: ")
-for concept in subsumers:
+for concept in result:
     print(formatter.format(concept))
 
 
